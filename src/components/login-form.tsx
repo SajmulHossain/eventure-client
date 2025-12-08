9b7356c54826a109b4518d87db7dedce54d49f50
Sajmul <sajmul1427@gmail.com>
2025-12-08 19:20:15 +0600

    login system done

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,27 +1,38 @@
 "use client";
 
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
 import {
   Field,
   FieldDescription,
   FieldGroup,
   FieldLabel,
-} from "@/components/ui/field"
-import { Input } from "@/components/ui/input"
-import Link from "next/link"
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+import { loginUser } from "@/services/auth/loginUser";
+import Link from "next/link";
+import { useActionState, useEffect } from "react";
+import { toast } from "sonner";
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const [state, formAction, isPending] = useActionState(loginUser, null);
+
+    useEffect(() => {
+      if (state && !state?.success && state?.message) {
+        toast.error(state.message);
+      }
+    }, [state]);
+  
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
@@ -32,16 +43,11 @@ export function LoginForm({
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
+          <form action={formAction}>
             <FieldGroup>
               <Field>
                 <FieldLabel htmlFor="email">Email</FieldLabel>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  
-                />
+                <Input id="email" type="email" placeholder="m@example.com" />
               </Field>
               <Field>
                 <div className="flex items-center">
@@ -50,12 +56,15 @@ export function LoginForm({
                 <Input id="password" type="password" placeholder="*******" />
               </Field>
               <Field>
-                <Button type="submit">Login</Button>
+                <Button disabled={isPending} type="submit">
+                  {isPending ? "Logging in..." : "Login"}
+                </Button>
                 <Button variant="outline" type="button">
                   Login with Google
                 </Button>
                 <FieldDescription className="text-center">
-                  Don&apos;t have an account? <Link href="/register">Sign up</Link>
+                  Don&apos;t have an account?{" "}
+                  <Link href="/register">Sign up</Link>
                 </FieldDescription>
               </Field>
             </FieldGroup>
@@ -63,5 +72,5 @@ export function LoginForm({
         </CardContent>
       </Card>
     </div>
-  )
+  );
 }
--- src/components/shared/Navbar/navbar.tsx
+++ src/components/shared/Navbar/navbar.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { NavMenu } from "@/components/shared/Navbar/nav-menu";
 import { NavigationSheet } from "@/components/shared/Navbar/navigation-sheet";
 import Logo from "@/assets/logo/Logo";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
@@ -14,12 +15,15 @@ const Navbar = () => {
 
         <div className="flex items-center gap-3">
           <Button
+            asChild
             variant="outline"
             className="hidden sm:inline-flex rounded-full"
           >
-            Log In
+            <Link href="/login">Log In</Link>
           </Button>
-          <Button className="rounded-full">Register</Button>
+          <Button asChild className="rounded-full">
+            <Link href="register">Register</Link>
+          </Button>
 
           {/* Mobile Menu */}
           <div className="md:hidden">
