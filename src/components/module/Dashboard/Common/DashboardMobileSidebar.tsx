ba9db607ab2bbb723671c6669372edd6dac5130d
Sajmul Hossain <sajmul1427@gmail.com>
2026-01-13 22:57:01 +0600

    mobile dashboard sidebar scrollable problem solved

--- src/components/module/Dashboard/Common/DashboardMobileSidebar.tsx
+++ src/components/module/Dashboard/Common/DashboardMobileSidebar.tsx
@@ -32,7 +32,7 @@ const DashboardMobileSidebar = ({
       <SheetTitle className="sr-only">Navigation Menu</SheetTitle>
 
       {/* Navigation */}
-      <ScrollArea className="flex-1 px-3 py-4">
+      <ScrollArea className="flex-1 px-3 py-4 overflow-y-auto">
         <nav className="space-y-6">
           {navItems.map((section, sectionIdx) => (
             <div key={sectionIdx}>
